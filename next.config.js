d3c4d5828f1c852067183808f1dccb446c31946a
Akash Singh <akashgandhi9595@gmail.com>
2026-01-12 17:41:23 -0500

    Successful static build (out ready)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
